dd314266ec21bfffeaebc57b48ecaeae96bb0e51
julianladisch <julianladisch@users.noreply.github.com>
2025-05-11 21:19:31 +0200

    Update EmptyMethodsCheckSample.java

--- java-checks-test-sources/default/src/main/java/checks/EmptyMethodsCheckSample.java
+++ java-checks-test-sources/default/src/main/java/checks/EmptyMethodsCheckSample.java
@@ -187,22 +187,18 @@ class EmptyMethodsCheckSample {
 
   private class ExceptionalCompliantCases {
     @org.aspectj.lang.annotation.Pointcut()
-    // Compliant
-    void foo() {
+    void foo() {  // Compliant
 
     }
 
     @Pointcut()
-    // Compliant
-    void bar() {}
+    void bar() {}  // Compliant
 
     @org.springframework.cache.annotation.CacheEvict(cacheNames = "flowers", allEntries = true)
-    // Compliant
-    void evictAll() {}
+    void evictAll() {}  // Compliant
 
     @CacheEvict(value = "flowers", key = "{#name}")
-    // Compliant
-    void evict(String name) {}
+    void evict(String name) {}  // Noncompliant
     
     @Before("")
     void stillTriggerOnOtherAnnotations() {} // Noncompliant
